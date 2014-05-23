ff2dc584b6ad64a4d80f0672f3d988a4612efb44
Tathagata Das <tathagata.das1565@gmail.com>
2014-05-23 08:12:41 -0700

    Fixed the JVM memory metric.

--- app_jar_project/src/main/scala/TestApp.scala
+++ app_jar_project/src/main/scala/TestApp.scala
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package main.scala
 
 import scala.collection.mutable.{ListBuffer, Queue}
@@ -46,13 +29,14 @@ object TestApp {
     println("\nSecondary JAR found at driver: true") 
     println("\nSecondary JAR found at executors: " + (result.toSet == (1 to 10).map(_ * 2).toSet))
     println("\nJava options set: [ property = " + System.getProperty("property") + " ]")
-    println("\nMemory size: " + (Runtime.getRuntime.totalMemory() / (1024 * 1024)) + " MB")
+    println("\nMemory size: " + (Runtime.getRuntime.maxMemory / (1024 * 1024)) + " MB")
     println("\nClasspath contains testdir: " + System.getProperty("java.class.path").contains("testdir"))
     println("\nCurrent workdir contains testfile: " + testFileExists)
     println("===========================================")
   }
 
   def main(args: Array[String]) {
+    println("spark.jars = " + System.getProperty("spark.jars"))
     val conf = new SparkConf().setAppName("TestApp")
     val sc = new SparkContext()
     test(sc)
